9b3197b3d38bc4632b44415ab8ec1792e3518762
mahmoodiftee <mahmoodiftee@gmail.com>
2023-12-10 03:54:54 +0600

    banner in the making

---
+++ src/Pages/HomePage/Banner.jsx
@@ -0,0 +1,16 @@
+
+const Banner = () => {
+    return (
+        <div className="flex">
+            <div className="flex-1 h-96 border flex items-center text-white">
+                <h1 className="text-7xl italic font-extrabold">
+                    Home For<span className="text-orange block ml-2">Every Paw</span>
+                </h1>
+            </div>
+            <div className="flex-1 h-96 border"></div>
+        </div>
+
+    );
+};
+
+export default Banner;
