d82cfc01dca5ebf7261b31aeed0b96726d5cd2fd
mahmoodiftee <mahmoodiftee@gmail.com>
2023-12-10 20:57:40 +0600

    created a global title

--- src/Pages/Available/Available.jsx
+++ src/Pages/Available/Available.jsx
@@ -4,6 +4,9 @@ const Available = () => {
     return (
         <div className="my-6 md:my-10 ">
             <Title head1={'Available'} head2={'For Adoption'}></Title>
+        <div className="  mt-2 md:mt-4">
+
+        </div>
         </div>
     );
 };
