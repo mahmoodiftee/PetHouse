f951dd0c0044cc7ff3b5c29be8c316eea3fc00d2
mahmoodiftee <mahmoodiftee@gmail.com>
2023-12-10 22:10:47 +0600

    added pets for available component

--- src/Pages/Available/Available.jsx
+++ src/Pages/Available/Available.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Title from "../../Components/Title/Title";
-import json from '../../assets/data.json';
+import json from "../../assets/data.json";
 const Available = () => {
     const [pets, setPets] = useState([]);
     useEffect(() => {
@@ -20,31 +20,25 @@ const Available = () => {
                             <div className="h-[80%] overflow-hidden w-full">
                                 <img src={pet.img} alt="" className="h-full cursor-pointer mx-auto object-contain transition-transform transform-gpu hover:scale-150" />
                             </div>
-                            <article className="flex max-w-xl flex-col items-start justify-between">
-                                <div className="flex items-center gap-x-4 text-xs">
+                            <article className="flex my-2 max-w-xl flex-col items-start justify-between">
+                                <div className="flex justify-between w-full mb-2 items-center gap-x-4 text-xs">
+                                    <p href="#" className="relative z-10 rounded-full px-1.5 py-2px text-[10px] font-medium text-white bg-orange hover:bg-orange hover:text-white">Puppy</p>
                                     <time datetime="2020-03-16" className="text-orange">Mar 16, 2020</time>
-                                    <a href="#" className="relative z-10 rounded-full px-3 py-1.5 font-medium text-orange hover:bg-gray-100">Marketing</a>
                                 </div>
                                 <div className="group relative">
-                                    <h3 className="mt-3 text-lg font-semibold leading-6 text-orange group-hover:text-orange">
-                                        <a href="#">
+                                    <h3 className=" text-lg font-semibold leading-6 text-orange group-hover:text-orange">
+                                        <p href="#">
                                             <span className="absolute inset-0"></span>
                                             {pet.desc.split(' ')[0]}
-                                        </a>
+                                        </p>
                                     </h3>
-                                    <p className="mt-2 text-sm leading-6 hidden md:block text-orange">
-                                        {`${(pet.desc).slice(0, 30)}...`}
-                                    </p>
-
+                                    <p className="mt-2 text-sm leading-6 hidden md:block text-gray-400">{pet.desc.split(' ').slice(0, 30).join(' ')}..</p>
                                 </div>
-                                <div className="relative mt-4 flex items-center gap-x-4">
-                                    <img src="https://tailwindcss.com/_next/static/media/guillermo-rauch.8a24ab88.jpg" alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+                                <div className="relative mt-3 flex items-center gap-x-4">
+                                    <img src="https://tailwindcss.com/_next/static/media/guillermo-rauch.8a24ab88.jpg" alt="" className="h-8 w-8 rounded-full bg-black" />
                                     <div className="text-sm leading-6">
-                                        <p class="font-semibold text-orange">
-                                            <a href="#">
-                                                <span class="absolute inset-0"></span>
-                                                John Doe
-                                            </a>
+                                        <p class="font-semibold text-[12px] text-orange">
+                                                John Doe 
                                         </p>
                                     </div>
                                 </div>
