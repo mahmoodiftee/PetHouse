8214848a9ba8c04a8d996ae93a6f56968c0d72e6
mahmoodiftee <mahmoodiftee@gmail.com>
2023-12-17 03:52:11 +0600

    new side bar with new design

--- src/Pages/Blogs/HomeLayout/HomeLayout.jsx
+++ src/Pages/Blogs/HomeLayout/HomeLayout.jsx
@@ -3,15 +3,17 @@ import Options from "./Options";
 
 const HomeLayout = () => {
     return (
-        <div className="min-h-screen grid grid-cols-1 md:border md:grid-cols-12 gap-6 items-center">
-            <div className="hidden p-6 md:block h-full md:col-span-3 w-full md:border md:bg-[#161616] rounded-lg">
+        <div className="min-h-screen grid grid-cols-1 md:grid-cols-12 gap-3 items-center">
+            <div className="hidden p-6 md:block h-full md:col-span-3 w-full md:border-2 md:border-[#292929] md:bg-[#161616] rounded-lg">
                 <Options />
             </div>
-            <div className="h-full md:col-span-6 w-full md:border md:bg-[#161616] rounded-lg">
+            <div className="h-full py-6 md:col-span-6 w-full md:border-2 md:border-[#292929] md:bg-[#161616] rounded-lg">
                 <Outlet></Outlet>
             </div>
-            <div className="hidden md:block h-full md:col-span-3 w-full md:border md:bg-[#161616] rounded-lg">
-                Right Side
+            <div className="hidden p-6 md:block h-full md:col-span-3 w-full md:border-2 md:border-[#292929] md:bg-[#161616] rounded-lg">
+                <div className="pt-16">
+                    Right Side
+                </div>
             </div>
         </div>
     );
--- src/Pages/Blogs/HomeLayout/Options.jsx
+++ src/Pages/Blogs/HomeLayout/Options.jsx
@@ -1,56 +1,123 @@
 import React, { useState } from 'react';
-
+import { Disclosure } from '@headlessui/react'
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
 const Options = () => {
     const [Selected, setSelected] = useState('All Post');
     console.log(Selected);
     const handleSelectedOption = (value) => {
         setSelected(value);
     }
-    return (
-        <div className='flex flex-col justify-center items-center gap-3'>
-            <div className="flex items-center justify-start w-full mb-4">
-                <label htmlFor="default-checkbox" className="text-lg underline uppercase font-bold  text-gray-900 dark:text-gray-300">Options</label>
-            </div>
-            <div className="flex items-center justify-start w-full mb-4">
-                <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">
-                    All Post
-                </label>
-                <input
-                    type='radio'
-                    onChange={() => handleSelectedOption('All Post')}
-                    checked={Selected === 'All Post'}
-                    className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
-                />
-            </div>
-
-            <div className="flex items-center justify-start w-full mb-4">
-                <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Cat & Kitten</label>
-                <input
-                    type='radio'
-                    onChange={() => handleSelectedOption('Cat & Kitten')}
-                    checked={Selected === 'Cat & Kitten'}
-                    className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
-                />
-            </div>
-            <div className="flex items-center justify-start w-full mb-4">
-                <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Dog & Puppy</label>
-                <input
-                    type='radio'
-                    onChange={() => handleSelectedOption('Dog & Puppy')}
-                    checked={Selected === 'Dog & Puppy'}
-                    className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
-                />
-            </div>
-            <div className="flex items-center justify-start w-full mb-4">
-                <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Questions</label>
-                <input
-                    type='radio'
-                    onChange={() => handleSelectedOption('Questions')}
-                    checked={Selected === 'Questions'}
-                    className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
-                />
+    return (<>
+        <div className="w-full pt-16">
+            <div className="mx-auto w-full max-w-md rounded-2xl p-2">
+                <Disclosure>
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-orange px-4 py-2 text-left text-sm font-medium text-white hover:bg-orange focus:outline-none focus-visible:ring focus-visible:ring-white/75">
+                                <span className='font-bold tracking-wider'>Blog Section</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'rotate-180 transform' : ''
+                                        } h-5 w-5 text-white`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-white">
+                                <div className="flex items-center justify-start mb-4">
+                                    <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">
+                                        All Post
+                                    </label>
+                                    <input
+                                        type='radio'
+                                        onChange={() => handleSelectedOption('All Post')}
+                                        checked={Selected === 'All Post'}
+                                        className="ms-2 w-4 h-4 cursor-pointer text-orange bg-gray-100-gray-300 rounded focus:ring-orange dark:focus:ring-orange dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
+                                    />
+                                </div>
+                                <div className="flex items-center justify-start mb-4">
+                                    <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Cat & Kitten</label>
+                                    <input
+                                        type='radio'
+                                        onChange={() => handleSelectedOption('Cat & Kitten')}
+                                        checked={Selected === 'Cat & Kitten'}
+                                        className="ms-2 w-4 h-4 cursor-pointer text-orange bg-gray-100-gray-300 rounded focus:ring-orange dark:focus:ring-orange dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
+                                    />
+                                </div>
+                                <div className="flex items-center justify-start mb-4">
+                                    <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Dog & Puppy</label>
+                                    <input
+                                        type='radio'
+                                        onChange={() => handleSelectedOption('Dog & Puppy')}
+                                        checked={Selected === 'Dog & Puppy'}
+                                        className="ms-2 w-4 h-4 cursor-pointer text-orange bg-gray-100-gray-300 rounded focus:ring-orange dark:focus:ring-orange dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
+                                    />
+                                </div>
+                                <div className="flex items-center justify-start mb-4">
+                                    <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Questions</label>
+                                    <input
+                                        type='radio'
+                                        onChange={() => handleSelectedOption('Questions')}
+                                        checked={Selected === 'Questions'}
+                                        className="ms-2 w-4 h-4 cursor-pointer text-orange bg-gray-100-gray-300 rounded focus:ring-orange dark:focus:ring-orange dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
+                                    />
+                                </div>
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+                <Disclosure as="div" className="mt-2">
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-orange px-4 py-2 text-left text-sm font-medium text-white hover:bg-orange focus:outline-none focus-visible:ring focus-visible:ring-white/75">
+                                <span className='font-bold tracking-wider'>Adoption Section</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'rotate-180 transform' : ''
+                                        } h-5 w-5 text-white`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-white">
+                                <div className="flex items-center justify-start mb-4">
+                                    <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">
+                                        Adoption Post
+                                    </label>
+                                    <input
+                                        type='radio'
+                                        onChange={() => handleSelectedOption('Adoption')}
+                                        checked={Selected === 'Adoption'}
+                                        className="ms-2 w-4 h-4 cursor-pointer text-orange bg-gray-100-gray-300 rounded focus:ring-orange dark:focus:ring-orange dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
+                                    />
+                                </div>
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+                <Disclosure as="div" className="mt-2">
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-orange px-4 py-2 text-left text-sm font-medium text-white hover:bg-orange focus:outline-none focus-visible:ring focus-visible:ring-white/75">
+                                <span className='font-bold tracking-wider'>Help Section</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'rotate-180 transform' : ''
+                                        } h-5 w-5 text-white`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-white">
+                                <div className="flex items-center justify-start mb-4">
+                                    <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">
+                                        Help Post
+                                    </label>
+                                    <input
+                                        type='radio'
+                                        onChange={() => handleSelectedOption('Help')}
+                                        checked={Selected === 'Help'}
+                                        className="ms-2 w-4 h-4 cursor-pointer text-orange bg-gray-100-gray-300 rounded focus:ring-orange dark:focus:ring-orange dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
+                                    />
+                                </div>
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
             </div>
         </div>
+    </>
     );
 };
 
