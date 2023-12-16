2fe8449beb9128e88975b5c835121e5e77bcc838
mahmoodiftee <mahmoodiftee@gmail.com>
2023-12-17 03:08:31 +0600

    done

--- src/Pages/Blogs/HomeLayout/HomeLayout.jsx
+++ src/Pages/Blogs/HomeLayout/HomeLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../BlogNav/Navbar";
 import Options from "./Options";
 
 const HomeLayout = () => {
--- src/Pages/Blogs/HomeLayout/Options.jsx
+++ src/Pages/Blogs/HomeLayout/Options.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
 const Options = () => {
-    const [SearchTerm, setSearchTerm] = useState('');
-    console.log(SearchTerm);
+    const [Selected, setSelected] = useState('All Post');
+    console.log(Selected);
+    const handleSelectedOption = (value) => {
+        setSelected(value);
+    }
     return (
         <div className='flex flex-col justify-center items-center gap-3'>
             <div className="flex items-center justify-start w-full mb-4">
@@ -10,13 +13,12 @@ const Options = () => {
             </div>
             <div className="flex items-center justify-start w-full mb-4">
                 <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">
-                All Post
+                    All Post
                 </label>
                 <input
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    id="default-checkbox"
-                    type="checkbox"
-                    value="All Post"
+                    type='radio'
+                    onChange={() => handleSelectedOption('All Post')}
+                    checked={Selected === 'All Post'}
                     className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
                 />
             </div>
@@ -24,30 +26,27 @@ const Options = () => {
             <div className="flex items-center justify-start w-full mb-4">
                 <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Cat & Kitten</label>
                 <input
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    id="default-checkbox"
-                    type="checkbox"
-                    value="Cat & Kitten"
+                    type='radio'
+                    onChange={() => handleSelectedOption('Cat & Kitten')}
+                    checked={Selected === 'Cat & Kitten'}
                     className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
                 />
             </div>
             <div className="flex items-center justify-start w-full mb-4">
                 <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Dog & Puppy</label>
                 <input
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    id="default-checkbox"
-                    type="checkbox"
-                    value="Dog & Puppy"
+                    type='radio'
+                    onChange={() => handleSelectedOption('Dog & Puppy')}
+                    checked={Selected === 'Dog & Puppy'}
                     className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
                 />
             </div>
             <div className="flex items-center justify-start w-full mb-4">
                 <label htmlFor="default-checkbox" className="text-sm font-medium text-gray-900 dark:text-gray-300">Questions</label>
                 <input
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    id="default-checkbox"
-                    type="checkbox"
-                    value="Questions"
+                    type='radio'
+                    onChange={() => handleSelectedOption('Questions')}
+                    checked={Selected === 'Questions'}
                     className="ms-2 w-4 h-4 cursor-pointer text-blue-600 bg-gray-100-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark-gray-600"
                 />
             </div>
