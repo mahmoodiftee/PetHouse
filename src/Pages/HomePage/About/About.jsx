4e4d91d517366e08e050a0e7d00890d30fa2467b
mahmoodiftee <mahmoodiftee@gmail.com>
2024-01-12 18:04:12 +0600

    update on about section

--- src/Pages/HomePage/About/About.jsx
+++ src/Pages/HomePage/About/About.jsx
@@ -1,27 +1,37 @@
 import Button from "../../../Components/Button/Button";
-import Title from "../../../Components/Title/Title";
 const About = () => {
     return (
-        <div className="relative overflow-hidden my-6 md:mb-10 md:mt-20">
-            <Title head1={'About'} head2={'Us'}></Title>
-            <div className="my-4 md:my-10">
-                <div className="relative mx-auto px-4 sm:static flex flex-col-reverse md:flex-row-reverse items-center">
-                    <div className="sm:max-w-[690px] flex-1">
-                        <h1 className="text-2xl md:text-5xl font-bold tracking-tight text-white">
-                            Providing Shelter, Love & Hope to Every Paw in Need
-                        </h1>
-                        <p className="mt-2 md:mt-4 tracking-tight text-white text-sm ">
-                            At Pet House, our commitment is to be a beacon of compassion for animals in distress. With unwavering dedication, we strive to create a haven where every paw, furry or feathered, finds a loving home. Our mission goes beyond sheltering – it's about restoring hope, healing wounds, and creating a brighter future for every four-legged companion. Join us in our journey to make the world a better place, one paw at a time.
-                        </p>
-                        <Button text={'Learn More..'} />
-                    </div>
-                    <div className="flex-1 mt-4 md:mt-0">
-                        <img className="h-full " src="https://i.ibb.co/6sDMgTj/about2-2.png" alt="" />
-                    </div>
-                </div>
+        <div className="flex flex-col-reverse md:flex-row relative mt-20 mb-28 md:mb-40 overflow-hidden justify-center md:h-[400px] mx-auto w-full">
+            <img className="hidden md:-mr-16 md:flex flex-1 md:flex-none h-full object-contain" src="https://i.ibb.co/6sDMgTj/about2-2.png" alt="" />
+            <div className='flex flex-col justify-center flex-1 md:flex-none md:w-[60%] items-center'>
+                <h1 className="text-3xl md:text-5xl text-orange my-2 font-extrabold">About <span className="text-white">Us</span></h1>
+                <h1 className="text-2xl text-center text-gray-300 md:text-xl font-bold tracking-tight">
+                    Providing <span className="text-orange">shelter, </span><span className="text-orange">love & </span><span className="text-orange">hope </span> to every paw in need.
+                </h1>
+                <p className="mt-2 tracking-tight text-center text-white text-lg ">
+                    At Pet House, our commitment is to be a beacon of compassion for animals in distress. With unwavering dedication, we strive to create a haven where every paw, furry or feathered, finds a loving home.
+                </p>
+                <Button text={'Learn More..'} />
             </div>
         </div>
     );
 };
 
 export default About;
+{/* <div className="my-4 md:my-10">
+<div className="relative mx-auto px-4 sm:static flex flex-col-reverse md:flex-row-reverse items-center">
+    <div className="sm:max-w-[690px] flex-1">
+    <h1 className="text-3xl md:text-5xl text-orange my-2 font-extrabold">About <span className="text-white">Us</span></h1>
+        <h1 className="text-2xl text-gray-300 md:text-xl font-bold tracking-tight">
+            Providing <span className="text-orange">shelter, </span><span className="text-orange">love & </span><span className="text-orange">hope </span> to every paw in need.
+        </h1>
+        <p className="mt-2 tracking-tight text-white text-lg ">
+            At Pet House, our commitment is to be a beacon of compassion for animals in distress. With unwavering dedication, we strive to create a haven where every paw, furry or feathered, finds a loving home.
+        </p>
+        <Button text={'Learn More..'} />
+    </div>
+    <div className="flex-1 mt-4 md:mt-0">
+        <img className="h-full " src="https://i.ibb.co/6sDMgTj/about2-2.png" alt="" />
+    </div>
+</div>
+</div> */}
--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import imageS from '../../../assets/bannerS.png';
 
 const Banner = () => {
     return (
-        <div className="flex relative md: justify-center overflow-hidden h-[400px] mx-auto w-full ">
+        <div className="flex relative justify-center overflow-hidden h-[400px] mx-auto w-full ">
             <div className='flex flex-1 md:flex-none items-center'>
                 <div className='absolute md:relative ml-3'>
                     <h1 className="text-lg md:text-7xl italic font-extrabold">
@@ -13,6 +13,7 @@ const Banner = () => {
                     <div className="flex justify-end items-center">
                         <Button text={'Adopt'} />
                     </div>
+                
                 </div>
             </div>
             <img className="flex flex-1 md:flex-none h-full object-contain" src={imageL} alt="" />
