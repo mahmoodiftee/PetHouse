8712faede1eddc9325e02b2d64734f7288a1f0ad
mahmoodiftee <mahmoodiftee@gmail.com>
2024-01-13 03:25:23 +0600

    ui structure for slider component done

--- src/Pages/HomePage/Available/SinglePosts/SingleAvailablePost.jsx
+++ src/Pages/HomePage/Available/SinglePosts/SingleAvailablePost.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 const SingleAvailablePost = () => {
     const post = useLoaderData();
-    const [isSignUpActive, setSignUpActive] = useState(true);
+    const [isSignUpActive, setSignUpActive] = useState();
 
     const handleSignUpClick = () => {
         setSignUpActive(true);
@@ -15,49 +15,30 @@ const SingleAvailablePost = () => {
     };
 
     return (
-        <div className="w-full flex justify-center items-center mx-auto my-10">
-            <div className={`container ${isSignUpActive ? 'right-panel-active' : ''}`}>
-                <div className="sign-up">
-                    <form action="#">
-                        <h1>Create Account</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <p>or use your email for registration</p>
-                        <input type="text" name="txt" placeholder="Name" required="" />
-                        <input type="email" name="email" placeholder="Email" required="" />
-                        <input type="password" name="pswd" placeholder="Password" required="" />
-                        <button>Sign Up</button>
-                    </form>
-                </div>
-                <div className="sign-in">
-                    <form action="#">
-                        <h1>Sign in</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <p>or use your account</p>
-                        <input type="email" name="email" placeholder="Email" required="" />
-                        <input type="password" name="pswd" placeholder="Password" required="" />
-                        <a href="#">Forget your Password?</a>
-                        <button>Sign In</button>
-                    </form>
-                </div>
-                <div className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>To keep connected with us, please login with your personal info</p>
-                            <button onClick={handleSignInClick}>Sign In</button>
-                        </div>
-                        <div className="overlay-right">
-                            <h1>Hello, Friend</h1>
-                            <p>Enter your personal details and start your journey with us</p>
-                            <button onClick={handleSignUpClick}>Sign Up</button>
+        <div className="w-full flex justify-center items-center mx-auto my-6">
+            <div className="hidden md:flex">
+                <div className={`Adoptioncontainer ${isSignUpActive ? 'right-panel-active' : ''}`}>
+                    <div className="sign-in">
+                        <form className="AdoptionForm" action="#">
+                            <h1>Image</h1>
+                        </form>
+                    </div>
+                    <div className="sign-up">
+                        <form className="AdoptionForm" action="#">
+                            <h1>Conditions</h1>
+                        </form>
+                    </div>
+                    <div className="overlay-container">
+                        <div className="overlay">
+                            <div className="overlay-left">
+                                <h1>Image</h1>
+                                <button className="Abutton" onClick={handleSignInClick}>Details</button>
+                            </div>
+                            <div className="overlay-right">
+                                <h1>Pet Details:</h1>
+                                <p>To keep connected with us, please login with your personal info</p>
+                                <button className="Abutton" onClick={handleSignUpClick}>Adopt</button>
+                            </div>
                         </div>
                     </div>
                 </div>
