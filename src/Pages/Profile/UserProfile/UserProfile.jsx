eeb5493e2ea307bc0847d55d45823dff69f17095
mahmoodiftee <mahmoodiftee@gmail.com>
2024-01-14 06:14:06 +0600

    desiging the user profile

--- src/Pages/Profile/UserProfile/UserProfile.jsx
+++ src/Pages/Profile/UserProfile/UserProfile.jsx
@@ -4,8 +4,10 @@ import { AuthContext } from "../../../Providers/AuthProvider";
 const UserProfile = () => {
     const { user } = useContext(AuthContext);
     return (
-        <div className="my-10">
-            <h1 className="text-center font-extrabold">Welcome <span className="text-orange">{user.displayName}</span></h1>
+        <div className="py-4 min-h-screen  w-full">
+            <div className="rounded-xl bg-dark h-[92vh] w-full">
+
+            </div>
         </div>
     );
 };
